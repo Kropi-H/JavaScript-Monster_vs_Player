1cc233f70c291a56695d497b9ee08b5e30d9499d
Kropi-H <kropacek.h@gmail.com>
2020-12-26 17:58:01 +0100

    using if statements for checking the win condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 8;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,8 +8,17 @@ let currentPlayerHealth = chosenMaxLife;
 adjustHealthBars(chosenMaxLife);
 
 function attackHandler() {
-const demage = dealMonsterDamage(ATTACK_VALUE);
-currentMonsterHealth -= demage; 
+  const demage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= demage;
+  const playerDemage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDemage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('Monster wons!'); 
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw!')
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
