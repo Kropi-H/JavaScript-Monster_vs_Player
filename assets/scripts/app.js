783c88a9d6bf259256af35b37b4d284a08003f08
Kropi-H <kropacek.h@gmail.com>
2020-12-27 09:48:58 +0100

    impruve if statements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,4 @@
-const ATTACK_VALUE = 10;
+const PLAYER_ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 8;
 
 let chosenMaxLife = 100;
@@ -8,14 +8,14 @@ let currentPlayerHealth = chosenMaxLife;
 adjustHealthBars(chosenMaxLife);
 
 function attackHandler() {
-  const demage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= demage;
+  const monsterDemage = dealMonsterDamage(PLAYER_ATTACK_VALUE);
+  currentMonsterHealth -= monsterDemage;
   const playerDemage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDemage;
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You won!');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert('Monster wons!'); 
+    alert('Monster wons!');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('You have a draw!')
   }
