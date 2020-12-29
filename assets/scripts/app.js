4218f1fd4985f5a16ffa0b37bbab2cf136cab44c
Kropi-H <kropacek.h@gmail.com>
2020-12-29 11:55:46 +0100

    the for-in loop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -242,9 +242,16 @@ function printLogHandler() {
   //   console.log(battleLog[i]);
   // };
 
+  let i = 0;
   for (const logEntry of battleLog) {
-    console.log(logEntry)
+    console.log(`#${i}`);
+    for (const key in logEntry){
+      console.log(`${key} => ${logEntry[key]}`);
+    }
+    i++;
   }
+
+
 
   // console.log(battleLog);
 }
